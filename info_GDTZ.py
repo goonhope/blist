42bd1523906bf97c677ea7273beac3003f7f514e
goonhope <50105128+goonhope@users.noreply.github.com>
2024-03-28 18:29:34 +0800

    Add files via upload

---
+++ info_GDTZ.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+@Filename	:	 info_GDTZ.py
+@Created 	:	2019/04/14
+@Updated	:	2022/06/19
+@Author   	:	goonhope@gmail.com;Teddy;Zhuhai
+@Function	:	投资、技改项目备案、节能评估数据抓取-for github
+"""
+from Search.gd import CNUM
+from require import open_txt,google_hder as ghdr,post_,excel,json_to_list,time_from,post_while
+import time, json,random
+
+
+@excel(na="list.xlsx",t=1)
+def get_all_list(city="珠海",num=1000,inv=True):
+    """获取投资备案或节能评估公示清单 From 20220709"""
+    pages,_,titles = get_ilist(city=city,inv=inv,num=num)
+    rows = get_ilist(pages=pages,city=city,num=num,inv=inv)
+    rows.insert(0,titles)
+    return rows
+
+
+def get_ilist(code="",city="珠海",inv=True,pages=0,num=1000,pr=None):
+    """获取投资备案或节能评估公示清单"""
+    url = r"https://gd.tzxm.gov.cn/tzxmspweb/api/publicityInformation/selectByPageBA" if inv else \
+            r"https://gd.tzxm.gov.cn/tzxmspweb/api/publicityInformation/selectJnscByPage"
+    hd = { #'Cookie': 'SESSIONID=517F1ADC412EA4F00A7AE122066C7E12',
+          'Host': 'gd.tzxm.gov.cn', 'Origin': 'https://gd.tzxm.gov.cn',"DNT":"1",'Content-Type': 'application/json;charset=UTF-8',
+          'Referer': 'https://gd.tzxm.gov.cn/PublicityInformation/PublicityHandlingResultsList.html',
+          'sec-ch-ua-mobile': '?0', 'sec-ch-ua-platform': '"Windows"', 'Sec-Fetch-Dest': 'empty',
+          'Sec-Fetch-Mode': 'cors', 'Sec-Fetch-Site': 'same-origin', 'X-Requested-With': 'XMLHttpRequest',
+           "sec-ch-ua": '''"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"'''}
+    log = f"invest{'' if inv else '_e'}.log"
+    log = f"_{city or 'all'}.".join(log.split("."))
+    city = CNUM[city][-1].strip("0") if city and city in CNUM else ""
+    data = {"flag": "1", "nameOrCode": code, "pageSize": num, "city": city, "pageNumber": 1}
+    if not pages:
+        info = post_(url, data=json.dumps(data), hdrs=ghdr(hd), proxy=pr)
+        info = info["data"]
+        print(len(info["list"]))
+        info = info["totalPage"], info["totalRow"],list(info["list"][0].keys())
+        print("@共{}页，{}条".format(*info))
+        return info
+    else:
+        hold, ohold, c = [], open_txt(file=log), False
+        for page in range(1,pages + 1):
+            data.update(dict(pageNumber=page))
+            print(page,end="\t")
+            info = post_while(url, data=json.dumps(data), hdrs=ghdr(hd), proxy=pr)
+            if info:
+                info = info["data"]["list"]
+                ky = "projectCode" if inv else "proofCode"
+                for y in info:
+                    if y[ky] and city in y[ky]:
+                        rows = [x.replace("\t", "") if isinstance(x,str) else x for x in y.values()]
+                        c = choose(ohold, rows[-2],c=-2)
+                        if c: break
+                        else: hold.extend([rows])
+            if c:break
+            time.sleep(random.uniform(2.6,4.618))
+        print("@查询完毕！")
+        if ohold: hold.extend(ohold)
+        open_txt(hold,file=log)
+        return hold
+
+
+@excel(na="list.xlsx",t=False)
+def get_blist(city="珠海",num=2000):
+    """获取技改备案公示清单 From 20220623"""
+    _,pages,titles = get_once(city=city,num=num)
+    rows = get_once(pages,city=city,num=num)
+    rows.insert(0,titles)
+    return rows
+
+
+def get_once(pages=0,num=2000,city="珠海"):
+    """广东技改备案系统公示抓取 js
+        var strftime = l => l.map((x,i)=>"56".includes(i)?fnow(x,0):x);
+        var toList =(y) => [Object.keys(y[0]), ...y.map(x=>strftime(Object.values(x)))].map(x=>x.toString()).join("\n");
+        var toCsv = async (b="", ext="json") => {var data = await llist(b).then(x=>toList(x)); fsave(data,"console",ext);};
+        var llist = async (b="", f=false) => { var hold = [], i = 0;
+            while (1) {
+                var url = window.location.href.slice(0,-1) + i;
+                var data =await fetch(url).then(r =>r.json()); // 加载选项数据
+                fdata = b? data.data.content.filter(x=>x.proCode && x.proCode.includes(b)): data.data.content;
+                hold.push(...fdata);
+                console.log("@Page " + i );
+                if(f || (data.data.page === data.data.pages -1)){break;};
+                i++;};
+	    return hold;};
+    """
+    url = f"http://210.76.81.107/api/recordNotice?size={str(num)}&page=0"
+    file = f"invest_blist_{city or 'all' }.log"
+    dist = CNUM[city][-1].strip("0") if city and city in CNUM else ""
+    if not pages:
+        info = json_to_list(url)["data"]
+        info = info["total"], info["pages"],list(info["content"][0].keys())
+        print("@共{1}页，共{0}条".format(*info))
+        return info
+    else:
+        hold, ohold ,c = [], open_txt(file=file), False
+        for page in range(pages):
+            urlx = url.strip("0") + str(page)
+            print(urlx)
+            info = json_to_list(urlx)
+            if info:
+                info = info["data"]['content']
+                for y in info:
+                    if y["proCode"] and dist in y["proCode"]:
+                        rows = [str(time_from(y[x] / 1000)).strip() if "Time" in x else str(y[x]).strip() for x in y]
+                        # print(rows)
+                        c = choose(ohold, rows[1])
+                        if c: break
+                        else: hold.extend([rows])
+            if c: break
+            time.sleep(random.uniform(1,1.618))
+        if ohold: hold.extend(ohold)
+        open_txt(hold,file=file)
+        return hold
+
+
+def choose(hold,stri,c=1,cl=3):
+    """判断循环终止条件"""
+    inkeys = [x[c] for x in hold[:cl]] if hold else []
+    return stri and any(x in stri for x in inkeys)
+
+
+if __name__ == '__main__':
+    get_blist()
+
---
+++ require.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+"""
+@Filename	:	require.py
+@Created 	:	2024/03/28 15:17:52
+@Updated	:	2024/03/28 15:17:52
+@Author   	:	goonhope@gmail.com; Teddy; Zhuhai
+@Function	:	required function ground
+@Process  	:	read -> found -> extract -> done
+@WitNote	:	by re
+@Reference :  Personal project
+"""
+from faker import Faker
+from functools import wraps
+from bs4 import BeautifulSoup
+import os, requests, cchardet, os.path as op, random, time
+
+
+def excel(dir="", na="", t=True, r=True):
+    """写入excel装饰器：xlsx or xls"""
+    def ex_cel(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start = time.time()
+            info = func(*args, **kwargs)
+            _time_ = time.strftime("_%Y%m%d_%H%M%S") if t else ""
+            titles = [x for x in list(kwargs.values()) + list(args) if x and isinstance(x, str) and len(x) < 6]
+            farg = (f"{titles[0]}_" if titles else "") + na or "file"
+            out = _time_.join(op.splitext(farg)) if ".xls" in na.lower() else f'{farg}{_time_}.xls'
+            file_out = op.join(dir or os.path.dirname(__file__), out)
+            excel_in(info, file_out, rc=r,creator="goonhope@gmail.com",title="备案信息")
+            print(f"@{func.__name__}:\t[Time:{time.time() - start : 0.1f}s]")
+            return info
+        return wrapper
+    return ex_cel
+
+
+def excel_in(info, file_out, rc=True,bg="ffffff",**kwargs):
+    """xlsx 写入—背景颜色可选"""
+    from openpyxl import Workbook,styles
+    wb = Workbook()
+    cfill = lambda x: styles.PatternFill("solid", fgColor=x)
+    table = wb.create_sheet('list', 0)
+    for row, rows in enumerate(info, 1):  # 行 y-->Row
+        if isinstance(rows, str): rows = rows.split("\t")  # 避免字符串被分解
+        for col, data in enumerate(rows, 1):  # 列 x--> Column
+            x = row, col
+            tcell = table.cell(*(x if rc else x[::-1]), value=data)
+            tcell.font = styles.Font("微软雅黑")
+            if bg.strip("f"): tcell.fill = cfill(bg)
+    if "Sheet" in wb.sheetnames: wb.remove(wb["Sheet"])
+    meta(wb.properties, **kwargs)
+    wb.save(file_out)
+
+
+def meta(prop, **kwargs):
+    """元信息修改"""
+    for x, y in kwargs.items():
+        if x in prop.__class__.__dict__ and prop.__getattribute__(x) != y:
+            prop.__setattr__(x, y)
+
+
+def google_hder(host=None, o=True):
+    """'google search url headers"""
+    google_hders = {
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN, zh;q=0.9',
+        'Connection': 'keep-alive',
+        'Referer': f'https://www.{"google.com.hk" if o else "qq.com"}',
+        'Upgrade-insecure-requests': '1',
+        'User-Agent': Faker("zh_CN").chrome()} # fr..user_agent()
+    if host and isinstance(host,(str,dict)):
+        google_hders.update(host if isinstance(host,dict) else dict(Host=host))
+    return google_hders
+
+
+def json_to_list(url, js=True, data=None):
+    """url json to xls or soup"""
+    url_headers = google_hder({'Host': url.split("/")[2]},False)
+    if data and isinstance(data, dict): url_headers.update(data)
+    dt = requests.get(url, headers=url_headers)
+    if dt.status_code == 200:
+        if js: return dt.json()
+        else:
+            dt.encoding = cchardet.detect(dt.content)['encoding']  # 确定网页编码 utf8 or GB18030
+            return BeautifulSoup(dt.text, 'lxml')
+    else: return False
+
+
+def open_txt(keys="", file="delaw.log", llst=True, encoding="utf-8"):
+    """读取txt 写文件，llist 1 读写二维list文件，0直接读写"""
+    file = op.join(os.path.dirname(__file__), file) if not os.path.isabs(file) else file
+    if keys:
+        with open(file, "w", encoding=encoding) as f:
+            f.write(list2d(keys) if llst else keys)
+    else:
+        if not op.exists(file): return []
+        else:
+            with open(file, "r", encoding=encoding) as f:
+                info = f.read()
+                return list2d(info) if llst else info
+
+
+def list2d(llist, stp=r"'\" ", sep="\t"):
+    """二维数列与字符串转换"""
+    if isinstance(llist, list):
+        return "\n".join(sep.join(str(x).strip(stp) for x in key) for key in llist)
+    elif isinstance(llist, str):
+        return [x.strip(stp).split(sep) for x in llist.strip().split("\n") if x.strip()] if llist else []
+    else:
+        return False
+
+
+def post_(url, hdrs, data=None, json=True, proxy=None):
+    """post"""
+    proxy = random.choice(proxy) if isinstance(proxy, list) else proxy
+    url_data = requests.post(url, data=data, headers=hdrs, timeout=3, proxies=proxy)
+    if url_data.status_code == 200:
+        url_data.encoding = cchardet.detect(url_data.content)['encoding']  # 网页编码utf8 or GB18030
+        return url_data.json() if json else BeautifulSoup(url_data.text, 'lxml')
+    else: return print("@post_ check !") or False
+
+
+def post_while(url, hdrs, data, json=True, proxy=None, max=5):
+    """post_while"""
+    times, ifs = 0, False
+    proxy = random.choice(proxy) if isinstance(proxy, list) else proxy
+    while not ifs and times < max:
+        try:
+            url_data = requests.post(url, data=data, headers=hdrs, timeout=3, proxies=proxy)
+            ifs = url_data.status_code == 200
+            if ifs and times == max:
+                url_encoding = cchardet.detect(url_data.content)['encoding']  # 确定网页原编码
+                url_data.encoding = url_encoding  # ' utf8' or "GB18030"
+                data_content = url_data.json() if json else BeautifulSoup(url_data.text, 'lxml')
+                return data_content
+        except:
+            times += 1; print(f"@post_while, Try: {str(times)} times !")
+            time.sleep(random.uniform(1, 3.14))
+
+
+def time_from(t=0, fmt="%Y%m%d %H:%M:%S"):
+    """时间相互转换，时间戳与字符串"""
+    if isinstance(t, (int, float)):
+        return time.strftime(fmt, time.localtime(t)) if t else time.strftime(fmt)
+    elif isinstance(t, str):
+        timeArray = time.strptime(t, fmt)
+        return int(time.mktime(timeArray))
+    else: return print("@t: int, float, str !") or False
+
+
+CNUM = {'广州': (200, '440100'), '韶关': (751, '440200'), '深圳': (755, '440300'),
+                '珠海': (756, '440400'), '汕头': (754, '440500'), '佛山': (757, '440600'),
+                '江门': (750, '440700'), '湛江': (759, '440800'), '茂名': (668, '440900'),
+                '肇庆': (758, '441200'), '惠州': (752, '441300'), '梅州': (753, '441400'),
+                '汕尾': (660, '441500'), '河源': (762, '441600'), '阳江': (662, '441700'),
+                '清远': (763, '441800'), '东莞': (769, '441900'), '中山': (760, '442000'),
+                '潮州': (768, '445100'), '揭阳': (663, '445200'), '云浮': (766, '445300')}
+
+
