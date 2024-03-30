6252c44247613a2e84e62e5cb302952eb479b0bd
goonhope <50105128+goonhope@users.noreply.github.com>
2024-03-30 11:15:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,17 @@
 @Function	:	获取广东备案信息
 @Reference	:	node.js -> add  "type": "module"  to package.json file
 */
+import fetch from "node-fetch";
+import xlsx from "node-xlsx";
+import fs from "fs";
 
-import fetch from "node-fetch"
 //const { JSDOM } = require("jsdom");
 var fnow = (now=0,i=1,a=8)=>{ 
 	var nnow =  new Date((now || Date.now()) + a * 3600 * 1000) ;
 	return i ? nnow.getTime() : nnow.toJSON().substr(0, 19).replace("T"," ")}; 
 var strftime = l => l.map((x,i)=>"56".includes(i)?fnow(x,0):x);
-var toList =(y) => [Object.keys(y[0]), ...y.map(x=>strftime(Object.values(x)))].map(x=>x.toString()).join("\n");
-var toCsv = async (b="", ext="csv") => await llist(b).then(x=>toList(x));
+var toList =(y) => [Object.keys(y[0]), ...y.map(x=>strftime(Object.values(x)))];
+var toCsv = async (b="", ext="csv") => await llist(b).then(x=>toList(x).map(x=>x.toString()).join("\n"));
 var llist = async (b="", f=false) => { var hold = [], i = 0;
     while (1) {
         var url = "http://210.76.81.107/api/recordNotice?size=2000&page=" + i;
@@ -26,7 +28,13 @@ var llist = async (b="", f=false) => { var hold = [], i = 0;
         i++;};
 return hold;};
 
-toCsv()
-	.then(x=>console.log(x))
-	.catch(err=>console.log(err))
-	.finally();
+var buffer = xlsx.build([{name: 'mySheetName', data: data}]);
+var toxls = (data, name="list.xlsx") => fs.writeFile(name, 
+			xlsx.build([{name: 'list', data: data}]), 
+			(err) => console.log(...(err?[err,"fail!"]:["done!"])));
+
+toxls().then().catch(err=>console.log(err)).finally()
+// toCsv()
+// 	.then(x=>console.log(x))
+// 	.catch(err=>console.log(err))
+// 	.finally();
